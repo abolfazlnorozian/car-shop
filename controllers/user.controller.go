3bbaccd3fe47033de9e9a727a35bdb256a4a6874
abolfazlnorozian <abolfazlnorozian7@gmail.com>
2022-08-28 13:45:23 +0430

    product/jwt

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -56,7 +56,6 @@ func (uc *Controller) RegistrUser(ctx *gin.Context) {
 	user.Password = &password
 	user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 	user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-
 	user.ID = primitive.NewObjectID()
 	user.User_id = user.ID.Hex()
 	token, refreshToken, err := services.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, user.User_id) //token ra ersal konim barye user
